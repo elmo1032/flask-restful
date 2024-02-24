508d355bb55255c272dcd520be11408fe774669d
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-23 21:57:52 -0500

    new commit for commenting

--- app/db.py
+++ app/db.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3  <-- specifies the interpreter to use for this script
+# -*- coding: utf-8 -*- <-- specifies the encoding of the script
+
 # standard python imports
+from flask_sqlalchemy import SQLAlchemy # imports the SQLAlchemy class from the flask_sqlalchemy library
 
-from flask_sqlalchemy import SQLAlchemy
-
+# create an instance of SQLAlchemy class
 db = SQLAlchemy()
 
