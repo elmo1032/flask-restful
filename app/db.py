0ebbe13bedf216666ca21d8e7d1e9da734ecd9b7
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-03-04 13:11:58 -0500

    comment

--- app/db.py
+++ app/db.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python3  <-- specifies the interpreter to use for this script
-# -*- coding: utf-8 -*- <-- specifies the encoding of the script
+#!/usr/bin/env python3  # specifies the interpreter to use for this script
+# -*- coding: utf-8 -*-  # specifies the encoding of the script
 
-# standard python imports
-from flask_sqlalchemy import SQLAlchemy # imports the SQLAlchemy class from the flask_sqlalchemy library
+# Import the SQLAlchemy class from the flask_sqlalchemy library
+from flask_sqlalchemy import SQLAlchemy
 
-# create an instance of SQLAlchemy class
+# Create an instance of the SQLAlchemy class
+# This creates a new SQLAlchemy object that will be used to interact with the database
 db = SQLAlchemy()
 
+# The db object can be used to define database models, query the database, and perform other database-related tasks
+# By creating the db object here, it can be easily reused throughout the application to interact with the database
