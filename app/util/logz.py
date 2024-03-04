ae7dbc4d51667c0ed9e2fa7e51c14b8b610db75a
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-03-04 13:19:29 -0500

    comment

--- app/util/logz.py
+++ app/util/logz.py
@@ -14,4 +14,27 @@ def create_logger():
 
     This function creates a logger instance that can be used to log messages
     throughout the application. The log level is determined by the 'LOGLEVEL'
+    environment variable. If the variable is not set, the default log level is
+    set to 'WARNING'. The logger instance is returned, and can be used to log
+    messages using the standard logging methods (e.g. debug(), info(), warning(),
+    error(), critical()). The logger instance also includes a RichHandler, which
+    formats log messages using the Rich library for better readability.
 
+    Returns:
+        logging.Logger: A logger instance that can be used to log messages
+        throughout the application.
+    """
+    log_level = logging.WARNING
+    if 'LOGLEVEL' in environ:
+        log_level = environ['LOGLEVEL'].upper()
+        log_level = getattr(logging, log_level, logging.WARNING)
+
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    logger.handlers = []
+
+    console = Console()
+    handler = RichHandler(console=console, rich_tracebacks=True)
+    logger.addHandler(handler)
+
+    return logger
