a1997f52ee73b19bd0f9d3d280aea85a692351a1
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-04-24 21:30:56 -0400

    comment

--- app/models/item.py
+++ app/models/item.py
@@ -12,32 +12,8 @@ class ItemModel(db.Model):
     __tablename__ = 'items'
 
     # Defining the columns of the table
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    price = db.Column(db.Float(precision=2))
+    id = db.Column(db.Integer, primary_key=True)  # Primary key column for the items table
+    name = db.Column(db.String(80))  # Column to store the name of the item
+    price = db.Column(db.Float(precision=2))  # Column to store the price of the item
 
-    # Defining a foreign key relationship with the StoreModel class
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
-    store = db.relationship('StoreModel')
 
-    # Initializing the ItemModel class with the given name, price, and store_id
-    def __init__(self, name, price, store_id):
-        self.name = name
-        self.price = price
-        self.store_id = store_id
-
-    # Defining a method to convert the ItemModel object to a JSON object
-    def json(self):
-        return {'name': self.name, 'price': self.price, 'store_id': self.store_id}
-
-    # Defining a class method to find an ItemModel object by name
-    @classmethod
-    def find_by_name(cls, name):
-        # Executing a SELECT query to find the first ItemModel object with the given name
-        return cls.query.filter_by(name=name).first()  # simple TOP 1 select
-
-    # Defining a method to save the ItemModel object to the database
-    def save_to_db(self):  # Upserting data
-        # Adding the ItemModel object to the database session and committing the changes
-        db.session.add(self)
-        db.session.commit()  # B
