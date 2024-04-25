f32f2a2e82a5450e712a4bec07ae2d0d343d761f
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-04-25 13:42:39 -0400

    improve

--- app/models/store.py
+++ app/models/store.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Import the database object from the app's db module
 from app.db import db
 
-# Define a new class StoreModel that inherits from db.Model
 class StoreModel(db.Model):
+    """Store model for storing store information in the database."""
 
-    # Set the name of the table in the database
     __tablename__ = 'stores'
-
-    # Define a column for the id, which is the primary key
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
 
-    # Define a column for the store name, with a maximum length of 80 characters
-    name = db.Column(db.String(80))
+    def __repr__(self):
+        """Provide a human-readable representation of the StoreModel object."""
+        return f'<StoreModel id={self.id} name={self.name}>'
