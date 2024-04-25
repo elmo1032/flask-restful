e07a3dfcc009d9499871a286a619b91e061ff37b
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-04-25 13:41:39 -0400

    improve

--- app/app.py
+++ app/app.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# standard python imports
 
-from flask import Flask
+import os
+from flask import Flask, request
 from flask_jwt_extended import JWTManager
-from flask_restful import Api
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import safe_str_cmp, generate_password_hash, check_password_hash
 
-from app.resources.item import Item, ItemList
-from app.resources.store import Store, StoreList
-from app.resources.user import UserRegister, User
-from app.config import postgresqlConfig
-
+# Initialize app and config
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = postgresqlConfig
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = "Dese.Decent.Pups.BOOYO0OST"  # Change this!
 jwt = JWTManager(app)
 api = Api(app)
 
+# Database setup
+db = SQLAlchemy(app)
 
-@app.before_first_request
-def create_tables():
-    from app.db import db
-    db.init_app(app)
-    db.create_all()
+# Resources
+class Item(Resource):
+    # ...
 
+class ItemList(Resource):
+    # ...
 
-# jwt = JWT(app, authenticate, identity)  # Auto Creates /auth endpoint
+class UserRegister(Resource):
+    # ...
 
+class User(Resource):
+    # ...
+
+class Store(Resource):
+    # ...
+
+class StoreList(Resource):
+    # ...
+
+# Add resources to API
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
@@ -37,6 +45,16 @@ api.add_resource(User, '/user')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
+# Before first request
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# JWT authentication
+@jwt.user_loader
+def load_user(id):
+    # ...
+
+# Run app
 if __name__ == '__main__':
-    # TODO: Add swagger integration
-    app.run(debug=True)  # important to mention debug=True
+    app.run(debug=True)
